eb35491cf66b379d896c2a808664a86e16e5950e
vivek <jk408mp@gmail.com>
2024-05-06 02:53:00 +0530

    Creating Array Constructor in js

---
+++ ArrayPlaylist_JS/Array_Constructor.js
@@ -0,0 +1,16 @@
+// Declaration of an empty array 
+// using Array constructor
+let District = new Array();
+console.log(District);
+ 
+// Creating and Initializing an array with values
+let courses = new Array("HTML", "CSS", "Javascript", "React","MongoDb","Node.Js","Express");
+console.log(courses[5]);
+console.log("Displaying Course Name=",courses);
+ 
+// Initializing Array while declaring
+let element = new Array(3);
+element[0] = 25;
+element[1] = 50;
+element[2] = 70;
+console.log(element);
---
+++ Loop/For_In_Loop.js
@@ -0,0 +1,5 @@
+let person=['raj','shyam','raju','vivek','sohan','ganesh']
+for(raj in person)
+{
+    console.log(person)
+}
