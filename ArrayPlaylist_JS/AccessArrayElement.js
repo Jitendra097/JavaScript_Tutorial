41cf563213d4942cbb5665526e7432dcd9a9d44c
vivek <jk408mp@gmail.com>
2024-05-06 02:44:07 +0530

    Creating an empty array

---
+++ ArrayPlaylist_JS/AccessArrayElement.js
@@ -0,0 +1,8 @@
+// Creating an Array and Initializing with Values
+let Technology=['html','CSS','Rect.js','MongoDB','Node.Js']
+
+console.log("Array element at index [1]:",Technology[1])   //Access Array element using index in javascript
+console.log("Array element at index [2]:",Technology[2])
+console.log("Array element at index [3]:",Technology[3])
+console.log("Array element at index [4]:",Technology[4])
+console.log("Array element at index []:",Technology[5])
---
+++ ArrayPlaylist_JS/Empty_Array.js
@@ -0,0 +1,2 @@
+let subject=[]                  //Creating a empty Array list
+console.log(subject)
