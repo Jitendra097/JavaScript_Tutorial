98a31dbd5384a5ac3748aeddcdf4fde80bb07610
vivek <jk408mp@gmail.com>
2024-05-05 19:13:06 +0530

    for loop using display natural number

---
+++ Loop/for_loop.js
@@ -0,0 +1,6 @@
+console.log("***************************************For Loop***************************************************")
+console.log("Natural Number is displaying on Screen")
+for(i=1;i<=100;i++)                                       //condition specified by for loop
+{
+    console.log(i)
+}
---
+++ Message.js
@@ -0,0 +1,2 @@
+console.log("Hello Everyone")
+console.log("I am so excited to learning of javascript")
