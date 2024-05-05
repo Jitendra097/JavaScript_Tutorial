b20b8d6213d0faaf5dcdc7e3a70e1b9a38385276
vivek <jk408mp@gmail.com>
2024-05-05 19:15:18 +0530

    Display natural number using While loop

---
+++ Loop/whileLoop.js
@@ -0,0 +1,8 @@
+console.log("***************************************While Loop***************************************************")
+let num_Start=1                                         //defined variable value using let keyword
+console.log("While loop using print Natural number:")
+while (num_Start<=20)                                   //condition specified by while loop
+{
+    console.log(num_Start)                               //function
+    num_Start+=1
+}
